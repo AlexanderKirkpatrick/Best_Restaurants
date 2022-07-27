9f3a207bc1510cea97a70a698034081ce42f7c2e
Alexander <knibz@protonmail.com>
2022-07-27 00:11:49 -0400

    add fields to review model, create mySQL table

--- Models/Review.cs
+++ Models/Review.cs
@@ -7,7 +7,7 @@ namespace Best_Restaurants.Models
   {
     public int ReviewId { get; set; }
     public string Description { get; set; }
-    public int CuisineId { get; set; }
+    public int RestaurantId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
     public virtual ICollection<Restaurant> Restaurants { get; set; }
   }
